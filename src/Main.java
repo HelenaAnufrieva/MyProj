cd6f376d5f501de809d24a130930ef95a7be7e01
HelenaAnufrieva <ghbdtnIM8>
2017-06-01 20:03:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Hillel2 on 01.06.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/New.java___jb_bak___
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Hillel2 on 01.06.2017.
+ */
+public class New {
+}
